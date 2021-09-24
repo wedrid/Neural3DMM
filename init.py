b05bf6a721d01baa872ad200bfda6496b99e3cf9
NikiDiCostanzo <niki.dicostanzo@stud.unifi.it>
2021-09-24 18:29:30 +0200

    Added script for creating folder hierarchy. Split jupyter file into multiple scripts.

--- init.py
+++ init.py
@@ -1,72 +1,88 @@
 from tqdm import tqdm
 import numpy as np
-import os, argparse
-
-def create_directory_hierarchy(): #TODO
-    pass
-
-def data_generation_func(root_dir, dataset, nVal):
-    name = ''
-
-    data = os.path.join(root_dir, dataset, 'preprocessed',name)
-    train = np.load(data+'/train.npy')
-
-    if not os.path.exists(os.path.join(data,'points_train')):
-        os.makedirs(os.path.join(data,'points_train'))
-
-    if not os.path.exists(os.path.join(data,'points_val')):
-        os.makedirs(os.path.join(data,'points_val'))
-
-    if not os.path.exists(os.path.join(data,'points_test')):
-        os.makedirs(os.path.join(data,'points_test'))
+import os
 
 
-    for i in tqdm(range(len(train)-nVal)):
-        np.save(os.path.join(data,'points_train','{0}.npy'.format(i)),train[i])
-    for i in range(len(train)-nVal,len(train)):
-        np.save(os.path.join(data,'points_val','{0}.npy'.format(i)),train[i])
-        
-    test = np.load(data+'/test.npy')
+def create_directory_hierarchy(args, dict_path):
+    if os.path.exists(os.path.join(args.root_dir)):
+        print('Created folders:')
+
+        if not os.path.exists(os.path.join(args.root_dir, 'preprocessed')):
+            os.makedirs(os.path.join(args.root_dir, 'preprocessed'))
+            print('\t\tProcessed')
+
+        if not os.path.exists(os.path.join(dict_path['downsample_directory'])):
+            os.makedirs(dict_path['downsample_directory'])
+            print(f'\t\tTemplate and downsample_directory')
+
+        ###
+        if not os.path.exists(os.path.join(dict_path['points_train_path'])):
+            os.makedirs(dict_path['points_train_path'])
+            print(f'\t\tPoints_train')
+
+        if not os.path.exists(dict_path['points_val_path']):
+            os.makedirs(dict_path['points_val_path'])
+            print('\t\tPoints_val')
+
+        if not os.path.exists(dict_path['points_test_path']):
+            os.makedirs(dict_path['points_test_path'])
+            print('\t\tPoints_test')
+
+        ###
+        if not os.path.exists(dict_path['summary_path']):
+            os.makedirs(dict_path['summary_path'])
+            print('\t\tSummary')
+
+        if not os.path.exists(dict_path['checkpoint_path']):
+            os.makedirs(dict_path['checkpoint_path'])
+            print('\t\tCheckpoint')
+
+        if not os.path.exists(dict_path['samples_path']):
+            os.makedirs(dict_path['samples_path'])
+            print('\t\tSamples')
+
+        if not os.path.exists(dict_path['prediction_path']):
+            os.makedirs(dict_path['prediction_path'])
+            print('\t\tPrediction')
+
+    else:
+        raise NotImplementedError('Create a root_dataset folder')
+
+
+def data_generation_func(args, dict_path, data):
+    train = np.load(data + '/train.npy')
+
+    for i in tqdm(range(len(train) - args.nVal)):
+        np.save(os.path.join(dict_path['points_train_path'], '{0}.npy'.format(i)), train[i])
+    for i in range(len(train) - args.nVal, len(train)):
+        np.save(os.path.join(dict_path['points_val_path'], '{0}.npy'.format(i)), train[i])
+
+    test = np.load(data + '/test.npy')
     for i in range(len(test)):
-        np.save(os.path.join(data,'points_test','{0}.npy'.format(i)),test[i])
-        
+        np.save(os.path.join(dict_path['points_test_path'], '{0}.npy'.format(i)), test[i])
+
     files = []
-    for r, d, f in os.walk(os.path.join(data,'points_train')):
+    for r, d, f in os.walk(dict_path['points_train_path']):
         for file in f:
             if '.npy' in file:
                 files.append(os.path.splitext(file)[0])
-    np.save(os.path.join(data,'paths_train.npy'),files)
+    np.save(os.path.join(data, 'paths_train.npy'), files)
 
     files = []
-    for r, d, f in os.walk(os.path.join(data,'points_val')):
+    for r, d, f in os.walk(dict_path['points_val_path']):
         for file in f:
             if '.npy' in file:
                 files.append(os.path.splitext(file)[0])
-    np.save(os.path.join(data,'paths_val.npy'),files)
+    np.save(os.path.join(data, 'paths_val.npy'), files)
 
     files = []
-    for r, d, f in os.walk(os.path.join(data,'points_test')):
+    for r, d, f in os.walk(dict_path['points_test_path']):
         for file in f:
             if '.npy' in file:
                 files.append(os.path.splitext(file)[0])
-    np.save(os.path.join(data,'paths_test.npy'),files)
+    np.save(os.path.join(data, 'paths_test.npy'), files)
 
 
-## beginning of the script
-
-## TODO creare funzione che crea la gerarchia delle cartelle cfr. https://github.com/gbouritsas/Neural3DMM
-'''
-parser = argparse.ArgumentParser(description='Arguments for dataset split')
-parser.add_argument('-r','--root_dir', type=str,
-            help='Root data directory location, should be same as in neural3dmm.ipynb')
-parser.add_argument('-d','--dataset', type=str, 
-            help='Dataset name, Default is DFAUST')
-parser.add_argument('-v','--num_valid', type=int, default=100, 
-            help='Number of meshes in validation set, default 100')
-
-args = parser.parse_args()
-'''
-def init_func():
-    create_directory_hierarchy()
-    data_generation_func(args.root_dir, args.dataset, args.num_valid)
-    
+def init_func(args, dict_path, data):
+    create_directory_hierarchy(args, dict_path)
+    data_generation_func(args, dict_path, data)
--- main.py
+++ main.py
@@ -1,2 +1,164 @@
 # Richiede di aver chiamato init.py una volta e create_downsampling_matrices.py almeno una volta.
 # fare solo il training, una volta che si hanno già le matrici di downsampling
+import os
+from init import init_func
+from train import train
+from test import test
+import torch
+from torch.utils.data import DataLoader
+from models import SpiralAutoencoder
+from autoencoder_dataset import autoencoder_dataset
+from transform_matrices import matrices_tr
+import argparse
+
+
+ds_factors = [4, 4, 4, 4]
+step_sizes = [2, 2, 1, 1, 1]
+filter_sizes_enc = [[3, 16, 32, 64, 128], [[], [], [], [], []]]
+filter_sizes_dec = [[128, 64, 32, 32, 16], [[], [], [], [], 3]]
+dilation_flag = True
+if dilation_flag:
+    dilation = [2, 2, 1, 1, 1]
+else:
+    dilation = None
+reference_points = [[414]]  # [[3567,4051,4597]] used for COMA with 3 disconnected components
+
+
+def main():
+    parser = argparse.ArgumentParser(description="neural 3DMM ...")
+    parser.add_argument("--GPU", dest="GPU", default=True, help="GPU is available")
+    parser.add_argument("--device", dest="device_idx", default='0', help="choose GPU")
+    parser.add_argument("--root_dir", dest="root_dir", default=None, help="Root data directory location")
+    #parser.add_argument("--dataset", dest="dataset_name", default=None, help="name of dataset folder ??")
+    parser.add_argument("--downsample_m", dest="downsample_method", default='COMA_downsample',
+                        help="Name of downsample method")
+    parser.add_argument("--generative_m", dest="generative_model", default='autoencoder', help="autoencoder... type ?")
+    parser.add_argument("--name", dest="name", default='', help="Name")
+    parser.add_argument("--num_v", dest="num_valid", default=100, help="Number valid for data_generation")
+    parser.add_argument("--seed", dest="seed", default=2, help="Seed ...")
+    parser.add_argument("--loss", dest="loss", default='l1', help="Loss type")
+    parser.add_argument("--batch_size", dest="batch_size", default=16, help="Batch size")
+    parser.add_argument("--epochs", dest="epochs", default=300, help="Number of epochs")
+    parser.add_argument("--eval_f", dest="eval_frequency", default=200, help="Eval frequency")
+    parser.add_argument("--num_w", dest="num_workers", default=4, help="Number of workers")
+    parser.add_argument("--filter_s_enc", dest="filter_sizes_enc", default=filter_sizes_enc,
+                        help="Sizes of filter encoder")
+    parser.add_argument("--filter_s_dec", dest="filter_sizes_dec", default=filter_sizes_dec,
+                        help="Sizes of filter decoder")
+    parser.add_argument("--nz", dest="nz", default=16, help="Nz")
+    parser.add_argument("--ds_factors", dest="ds_factors", default=ds_factors, help="ds_factors")
+    parser.add_argument("--step_sizes", dest="step_sizes", default=step_sizes, help="step_sizes")
+    parser.add_argument("--dilation", dest="dilation", default=dilation, help="dilation")
+    parser.add_argument("--lr", dest="lr", default=1e-3, help="lr")
+    parser.add_argument("--regularization", dest="regularization", default=5e-5, help="Regularization")
+    parser.add_argument("--scheduler", dest="scheduler", default=True, help="Scheduler")
+    parser.add_argument("--decay_rate", dest="decay_rate", default=0.99, help="decay_rate")
+    parser.add_argument("--decay_steps", dest="decay_steps", default=1, help="decay_steps")
+    parser.add_argument("--resume", dest="resume", default=False, help="decay_steps")
+    parser.add_argument("--mode", dest="mode", default='train', help="Mode")
+    parser.add_argument("--shuffle", dest="shuffle", default=True, help="Shuffle")
+    parser.add_argument("--nVal", dest="nVal", default=100, help="nVal")
+    parser.add_argument("--normalization", dest="normalization", default=True, help="Normalization")
+    parser.add_argument("--decay_rate", dest="decay_rate", default=0.99, help="decay_rate")
+    parser.add_argument("--checkpoint_file", dest="checkpoint_file", default='checkpoint', help="checkpoint_file")
+
+    args = parser.parse_args()
+
+    GPU = args.GPU
+    device_idx = args.device_idx
+    torch.cuda.get_device_name(device_idx)
+
+    results_folder = os.path.join(args.root_dir, 'results/spirals_ ' + args.generative_model)
+    data = os.path.join(args.root_dir, 'preprocessed', args.name)
+    dict_path = {
+        'reference_mesh_file': os.path.join(args.root_dir, 'template', 'template.obj'),
+        'downsample_directory': os.path.join(args.root_dir, 'template', args.downsample_method),
+        'summary_path': os.path.join(results_folder, 'summaries', args.name),
+        'checkpoint_path': os.path.join(results_folder, 'checkpoints', args.name),
+        'samples_path': os.path.join(results_folder, 'samples', args.name),
+        'prediction_path': os.path.join(results_folder, 'predictions', args.name),
+        'points_train_path': os.path.join(data, 'points_train'),
+        'points_val_path': os.path.join(data, 'points_val'),
+        'points_test_path': os.path.join(data, 'points_test')
+    }
+
+    # Folder and data_generation
+    init_func(args, dict_path, data)
+    if os.path.exists(data +'/mean.npy') or not os.path.exists(data+'/std.npy'):
+        shapedata, sizes, bU, bD, spirals_np, spiral_sizes, spirals = matrices_tr(args, reference_points)
+        # Model
+        torch.manual_seed(args.seed)
+
+        if GPU:
+            device = torch.device("cuda:" + str(device_idx) if torch.cuda.is_available() else "cpu")
+        else:
+            device = torch.device("cpu")
+        print(device)
+
+        tspirals = [torch.from_numpy(s).long().to(device) for s in spirals_np]
+        tD = [torch.from_numpy(s).float().to(device) for s in bD]
+        tU = [torch.from_numpy(s).float().to(device) for s in bU]
+
+        # Building model, optimizer, and loss function
+        dataset_train = autoencoder_dataset(root_dir=data, points_dataset='train',
+                                            shapedata=shapedata,
+                                            normalization=args.normalization)
+
+        dataloader_train = DataLoader(dataset_train, batch_size=args.batch_size,
+                                      shuffle=args.shuffle, num_workers=args.num_workers)
+
+        dataset_val = autoencoder_dataset(root_dir=data, points_dataset='val',
+                                          shapedata=shapedata,
+                                          normalization=args.normalization)
+
+        dataloader_val = DataLoader(dataset_val, batch_size=args.batch_size,
+                                    shuffle=False, num_workers=args.num_workers)
+
+        dataset_test = autoencoder_dataset(root_dir=data, points_dataset='test',
+                                           shapedata=shapedata,
+                                           normalization=args.normalization)
+
+        dataloader_test = DataLoader(dataset_test, batch_size=args.batch_size,
+                                     shuffle=False, num_workers=args.num_workers)
+
+        if 'autoencoder' in args.generative_model:
+            model = SpiralAutoencoder(filters_enc=args.filter_sizes_enc,
+                                      filters_dec=args.filter_sizes_dec,
+                                      latent_size=args.nz,
+                                      sizes=sizes,
+                                      spiral_sizes=spiral_sizes,
+                                      spirals=tspirals,
+                                      D=tD, U=tU, device=device).to(device)
+
+        optim = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.regularization)
+        if args.scheduler:
+            scheduler = torch.optim.lr_scheduler.StepLR(optim, args.decay_steps, gamma=args.decay_rate)
+        else:
+            scheduler = None
+
+        if args.loss == 'l1':
+            def loss_l1(outputs, targets):
+                L = torch.abs(outputs - targets).mean()
+                return L
+
+            loss_fn = loss_l1
+
+        # %%
+        params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+        print("Total number of parameters is: {}".format(params))
+        print(model)
+        # print(M[4].v.shape)
+
+        # Train
+        if args.mode == 'train':
+            train(model, device, dataloader_train, dataloader_val, optim, loss_fn, args, dict_path,
+                  scheduler, shapedata)
+        # Tests
+        elif args.mode == 'test':
+            test(model, device, args, dict_path, dataloader_test, shapedata)
+    else:
+        raise NotImplementedError('Run script create_downsampling_matrices.py first to generate file')
+
+
+if __name__ == '__main__':
+    main()
